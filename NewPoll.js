70db6e0cec7d153c73cb14b75849940b2409c22e
Aphenphos <MaxwillWinters@gmail.com>
2022-06-13 19:52:49 -0400

    adding form grabber;

---
+++ NewPoll.js
@@ -0,0 +1,14 @@
+function createNewPoll(){
+
+    const handleNewPoll = handlers.handleNewPoll;
+
+    const form = root.querySelector('form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        handleNewPoll(formData.get('pollQuestion'), formData.get('optionA'), formData.get('optionB'));
+
+        form.reset();
+    });}
